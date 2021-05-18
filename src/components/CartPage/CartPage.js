2ceb214d1b2a8b6e7d69f285364eb6bb82ba1850
Máté Groholy <mate.groholy@gmail.com>
2021-05-18 18:25:15 +0200

    feat: use CartContext to display Cart products in CartPage.

--- src/components/CartPage/CartPage.js
+++ src/components/CartPage/CartPage.js
@@ -6,7 +6,9 @@ import {
   makeStyles,
 } from "@material-ui/core";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
 import CartCard from "./CartCard";
 import EmptyCartCard from "./EmptyCartCard";
 
@@ -23,16 +25,7 @@ export const CART_API_URL = "http://localhost:8762/cart";
 const CartPage = () => {
   const classes = useStyles();
 
-  const [cartProducts, setCartProducts] = useState([]);
-
-  const fetchCartProducts = async () => {
-    try {
-      const response = await axios.get(CART_API_URL);
-      setCartProducts(response.data.products);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const { cartProducts } = useContext(CartContext);
 
   const calculateCartTotal = () => {
     let sum = 0;
@@ -47,14 +40,11 @@ const CartPage = () => {
       const response = await axios.delete(
         `${CART_API_URL}/remove/${productId}`
       );
-      fetchCartProducts();
       console.log(response);
     } catch (error) {
       console.log(error);
     }
   };
-
-  useEffect(() => fetchCartProducts(), []);
 
   return (
     <Container>
