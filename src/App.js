4bf7524614ee54c39fe8f8bfdb9ee237027b6540
Kriszti Toth <k95toth@gmail.com>
2021-05-18 15:59:54 +0200

    feat: add route for ProductCatalog component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import ProductCatalog from "./components/ProductService/ProductCatalog";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar />
+        <Switch>
+          <Route exact path="/" component={ProductCatalog} />
+          <Route exact path="/products" component={ProductCatalog} />
+        </Switch>
       </Router>
     </div>
   );
