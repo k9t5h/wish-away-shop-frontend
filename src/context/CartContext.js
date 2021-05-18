edbc124a040d37bc61e869c1dd8fa453f97ec767
Máté Groholy <mate.groholy@gmail.com>
2021-05-18 18:22:13 +0200

    refactor: remove unused React import.

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { CART_API_URL } from "../components/CartPage/CartPage";
 
 export const CartContext = createContext(null);
