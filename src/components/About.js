0b1bd52e5d4775babbfc7b446a3149990e2ee27b
Kriszti Toth <k95toth@gmail.com>
2021-05-19 19:00:00 +0200

    feat: create empty Paper component with whitesmoke background

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,42 @@
 import React from "react";
+import {
+  makeStyles,
+  MuiThemeProvider,
+  createMuiTheme,
+} from "@material-ui/core/styles";
+import { CssBaseline, Paper } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      default: "#f5f5f5",
+    },
+  },
+});
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: "50px auto",
+      width: "500px",
+      height: "500px",
+    },
+  },
+}));
 
 const About = () => {
-  return <div></div>;
+  const classes = useStyles();
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className={classes.root}>
+        <Paper elevation={3} />
+      </div>
+    </MuiThemeProvider>
+  );
 };
 
 export default About;
