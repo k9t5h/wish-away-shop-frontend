9090b7c69509f70e968c8716f20131b91447fb2f
Kriszti Toth <k95toth@gmail.com>
2021-05-19 17:50:05 +0200

    feat: add Route for OrderHistory component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import CartPage from "./components/CartPage/CartPage";
 import NavBar from "./components/NavBar";
+import OrderHistory from "./components/OrderService/OrderHistory";
 import ProductCatalog from "./components/ProductService/ProductCatalog";
 import CartContextHandler, { CartContext } from "./context/CartContext";
 
@@ -17,6 +18,7 @@ function App() {
             <Route exact path="/" component={ProductCatalog} />
             <Route exact path="/products" component={ProductCatalog} />
             <Route path={"/cart"} component={CartPage} />
+            <Route exact path={"/order"} component={OrderHistory} />
           </Switch>
         </Router>
       </CartContext.Provider>
