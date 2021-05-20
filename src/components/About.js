5ba17f694d1d2ecd84c81d2bb6b7ed0eca6b974f
Kriszti Toth <k95toth@gmail.com>
2021-05-20 09:41:14 +0200

    fix: remove whitesmoke background from About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,6 @@
 import React from "react";
-import {
-  makeStyles,
-  MuiThemeProvider,
-  createMuiTheme,
-} from "@material-ui/core/styles";
-import { CardMedia, CssBaseline, Paper, Typography } from "@material-ui/core";
-
-const theme = createMuiTheme({
-  palette: {
-    background: {
-      default: "#f5f5f5",
-    },
-  },
-});
+import { makeStyles } from "@material-ui/core/styles";
+import { CardMedia, Paper, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -40,36 +28,33 @@ const About = () => {
   const classes = useStyles();
 
   return (
-    <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      <div className={classes.root}>
-        <Paper elevation={3}>
-          <div className={classes.text}>
-            <Typography className={classes.title} variant="h4">
-              Welcome, Wanderer!
-            </Typography>
-            <Typography className={classes.description} variant="h5">
-              Have you ever received a present that you didn't like? This site
-              is for you!
-            </Typography>
-            <Typography variant="h6">
-              You can upload the present and others will buy it. And you can buy
-              others' stuff as well.
-            </Typography>
-            <Typography className={classes.description} variant="h6">
-              It's perfect since it not only redistributes things and gadgets
-              well (and so being environmentally friendly) but it redistributes
-              happiness as well!
-            </Typography>
-          </div>
+    <div className={classes.root}>
+      <Paper elevation={3}>
+        <div className={classes.text}>
+          <Typography className={classes.title} variant="h4">
+            Welcome, Wanderer!
+          </Typography>
+          <Typography className={classes.description} variant="h5">
+            Have you ever received a present that you didn't like? This site is
+            for you!
+          </Typography>
+          <Typography variant="h6">
+            You can upload the present and others will buy it. And you can buy
+            others' stuff as well.
+          </Typography>
+          <Typography className={classes.description} variant="h6">
+            It's perfect since it not only redistributes things and gadgets well
+            (and so being environmentally friendly) but it redistributes
+            happiness as well!
+          </Typography>
+        </div>
 
-          <CardMedia
-            component="img"
-            src="https://image.freepik.com/free-photo/close-up-young-couple-spending-time-together_273609-40436.jpg"
-          />
-        </Paper>
-      </div>
-    </MuiThemeProvider>
+        <CardMedia
+          component="img"
+          src="https://image.freepik.com/free-photo/close-up-young-couple-spending-time-together_273609-40436.jpg"
+        />
+      </Paper>
+    </div>
   );
 };
 
